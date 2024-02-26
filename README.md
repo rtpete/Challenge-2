6d9565961e3dfd87cc75a2d48df54791e3869879
Rachael Petersen <rachaeltpetersen@rachaelsmacbook.mynetworksettings.com>
2024-02-25 20:38:18 -0500

    working and documented solution

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# challenge2
+# Ant on a Grid Simulation
+
+This code is a solution to the given Challenge #2:
+> An ant is sitting on a grid of white and black squares.  Initially the grid is all white and the ant faces right.  
+At each step it does the following:  
+At a white square, flip the color of the square, turn 90 degrees right (clockwise), and move forward one unit.  
+At a black square, flip the color of the square, turn 90 degrees left (counterclockwise), and move forward one unit.  
+Write a program in Python to simulate the first K moves that the ant makes and display the grid at each move on an interface.  The only input to the method is the number of moves K.  
+
+## Usage
+
+1. Clone the repository.
+
+2. Ensure you have Python 3.9 installed.
+
+3. Run the script with Python and provide the number of steps as a command line argument.
+
+For example:
+~~~
+python ant_on_a_grid.py 50
+~~~
---
+++ ant_on_a_grid.py
@@ -0,0 +1,120 @@
+import turtle
+import sys
+
+
+class AntOnAGrid:
+    def __init__(self, K):
+        """
+        Initializes the AntOnAGrid class and defines all functionality necessary to run the simulation
+
+        Args:
+            K (int): The number of steps the ant will take
+        """
+        self.k = int(K)
+
+        # Initialize the screen window
+        self.window = turtle.Screen()
+        self.window.bgcolor('white')
+        self.window.screensize(600, 600)
+
+        # Initialize the grid to track square colors
+        self.grid = {}
+
+        # Initialize the turtle representing the ant on the square
+        self.ant_on_square = turtle.Turtle()
+        self.ant_on_square.shape('square')
+        self.ant_on_square.shapesize(1)
+        self.ant_on_square.speed('fastest')
+
+        # Set the step size for each move of the ant
+        self.ant_step = 20
+
+        # Initialize the position variable to track the ant's coordinates on the grid
+        self.position = self.get_ant_coordinates(self.ant_on_square)
+
+        # Start the ant's movement
+        self.move_ant(self.k, self.position, self.grid, self.ant_on_square, self.ant_step)
+
+    def move_ant(self, k: int, position: tuple, grid: dict, ant: turtle.Turtle, step: int):
+        """
+        Move the ant on the grid for a specified number of steps
+
+        Args:
+            k (int): Number of steps for the ant to take
+            position (tuple): Current coordinates of the ant
+            grid (dict): Dictionary to track square colors on the grid
+            ant (turtle.Turtle): Turtle representing the ant
+            step (int): Size of each step the ant takes
+        """
+        # Only move K number of steps
+        for _ in range(k):
+            if position not in grid or grid[position] == 'white':
+
+                # If the ant is on a white square, change the square to black
+                ant.fillcolor('black')
+
+                # Add the black square to the grid
+                ant.stamp()
+                self.change_square_color(grid, ant, 'black')
+
+                # If the ant is on a white square, turn clockwise 90 degrees and move forward
+                ant.right(90)
+                ant.forward(step)
+
+                # Update the position variable to evaluate the next step
+                position = self.get_ant_coordinates(ant)
+
+            elif grid[position] == 'black':
+
+                # If the ant is on a black square, change the square to white
+                ant.fillcolor('white')
+                self.change_square_color(grid, ant, 'white')
+
+                ant.stamp()
+                # If the ant is on a black square, turn counter-clockwise 90 degrees and move forward
+                ant.left(90)
+                ant.forward(step)
+
+                # Update the position variable to evaluate the next step
+                position = self.get_ant_coordinates(ant)
+
+    def change_square_color(self, grid: dict, ant: turtle.Turtle, color: str):
+        """
+        Change the color of the square the ant is on and update the grid.
+
+        Args:
+            grid (dict): Dictionary to track square colors on the grid
+            ant (turtle.Turtle): Turtle representing the ant
+            color (str): Color to set the square
+        """
+        grid[self.get_ant_coordinates(ant)] = color
+
+    def get_ant_coordinates(self, ant: turtle.Turtle) -> tuple:
+        """
+        Get the current coordinates of the ant.
+
+        Args:
+            ant (turtle.Turtle): Turtle representing the ant
+
+        Returns:
+            tuple: Current coordinates of the ant
+        """
+        ant_coordinates = (round(ant.xcor()), round(ant.ycor()))
+        return ant_coordinates
+
+
+if __name__ == "__main__":
+    # Get the number of steps from the command line input
+    try:
+
+        number_of_steps = int(sys.argv[1])
+    except Exception as e:
+        print(f"Error: {e}")
+        print("Enter a positive integer k in the following format: python ant_on_a_grid.py {k}")
+        sys.exit(1)
+
+    # Begin the simulation
+    ant = AntOnAGrid(number_of_steps)
+
+    # Keep the window open at the end of the simulation
+    turtle.done()
